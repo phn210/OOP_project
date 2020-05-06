dffe8baa252c209322ad8f93aaa49130ae04819f
hhrecode001 <57402661+hhrecode001@users.noreply.github.com>
2020-05-07 01:52:01 +0700

    Create LopCoPhieu.java

---
+++ LopCoPhieu.java
@@ -0,0 +1,29 @@
+package bai_tap_lon;
+
+public class LopCoPhieu{
+	public float CK;
+	public float TC;
+	public float Tran;
+	public float San;
+	public float MuaGia1;
+	public float MuaKL1;
+	public float MuaGia2;
+	public float MuaKL2;
+	public float MuaGia3;
+	public float MuaKL3;
+	public float SDCGia;
+	public float SDCKL;
+	public float Biendong;
+	public float BanGia1;
+	public float BanKL1;
+	public float BanGia2;
+	public float BanKL2;
+	public float BanGia3;
+	public float BanKL3;
+	public float LSCao;
+	public float LSThap;
+	public float TKL;
+	public float NNMua;
+	public float NNBan;
+	public float RoomCon;
+}
