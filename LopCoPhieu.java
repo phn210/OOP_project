2068b927778444e17274d490db7958c7d1dcaef7
hhrecode001 <57402661+hhrecode001@users.noreply.github.com>
2020-05-09 02:25:10 +0700

    thêm phương thức ghi ra thông tin

--- LopCoPhieu.java
+++ LopCoPhieu.java
@@ -1,7 +1,6 @@
 package bai_tap_lon;
-
 public class LopCoPhieu{
-	public float CK;
+	public String CK;
 	public float TC;
 	public float Tran;
 	public float San;
@@ -26,4 +25,33 @@ public class LopCoPhieu{
 	public float NNMua;
 	public float NNBan;
 	public float RoomCon;
+	
+	public void GhiThongTinCoPhieu() { // ghi ra toan bo thong tin cua ma co phieu
+		String tt = this.CK + " ";
+		tt += String.format("%.2f", this.TC) + " ";
+		tt += String.format("%.2f", this.Tran) + " ";
+		tt += String.format("%.2f", this.San) + " ";
+		tt += String.format("%.2f", this.MuaGia1) + " ";
+		tt += String.format("%.2f", this.MuaKL1) + " ";
+		tt += String.format("%.2f", this.MuaGia2) + " ";
+		tt += String.format("%.2f", this.MuaKL2) + " ";
+		tt += String.format("%.2f", this.MuaGia3) + " ";
+		tt += String.format("%.2f", this.MuaKL3) + " ";
+		tt += String.format("%.2f", this.SDCGia) + " ";
+		tt += String.format("%.2f", this.SDCKL) + " ";
+		tt += String.format("%.2f", this.Biendong) + " ";
+		tt += String.format("%.2f", this.BanGia1) + " ";
+		tt += String.format("%.2f", this.BanKL1) + " ";
+		tt += String.format("%.2f", this.BanGia2) + " ";
+		tt += String.format("%.2f", this.BanKL2) + " ";
+		tt += String.format("%.2f", this.BanGia3) + " ";
+		tt += String.format("%.2f", this.BanKL3) + " ";
+		tt += String.format("%.2f", this.LSCao) + " ";
+		tt += String.format("%.2f", this.LSThap) + " ";
+		tt += String.format("%.2f", this.TKL) + " ";
+		tt += String.format("%.2f", this.NNMua) + " ";
+		tt += String.format("%.2f", this.NNBan) + " ";
+		tt += String.format("%.2f", this.RoomCon) + " ";
+		System.out.println(tt);	
+	}
 }
