dacb1ad2698a02e614cb22f5ecdb6d0b43f5310a
hhrecode001 <57402661+hhrecode001@users.noreply.github.com>
2020-06-04 00:55:10 +0700

    Create NhomCoPhieu.java
    lưu các cổ phiếu cùng nhóm

---
+++ NhomCoPhieu.java
@@ -0,0 +1,49 @@
+package bai_tap_lon;
+
+import java.util.List;
+
+public class NhomCoPhieu{
+	private String tenNhom;
+	private List<CoPhieu> danhSachCP;
+	private float kiVong;
+	private float doLechChuan;
+	//contructor
+	public NhomCoPhieu(List<CoPhieu> dscp,String tenNhom ,String...danhSachMa ) {
+		this.setTenNhom(tenNhom);
+		for(String maCP:danhSachMa)
+		{
+			for(CoPhieu item: dscp) {
+				if (maCP==item.getTen())
+						danhSachCP.add(item);
+				
+			}
+		}
+	}
+	public String getTenNhom() {
+		return tenNhom;
+	}
+	public void setTenNhom(String tenNhom) {
+		this.tenNhom = tenNhom;
+	}
+	
+	public boolean isTang() { // kiem tra xem nhom co phieu day nhin chung co tang hay khong
+		int dem = 0;
+		for(CoPhieu coPhieu: danhSachCP) 
+			if (coPhieu.getBienDong()>0.02) dem++;
+		if ( ((float)dem / danhSachCP.size()) > 0.7 )
+			return true;
+		return false;
+	}
+	
+	public CoPhieu daiDienTang() { // dua ra dai dien tang nhieu nhat
+		CoPhieu tmp = null;
+		for(CoPhieu coPhieu: danhSachCP) 
+			if (coPhieu.getBienDong()>0.02) {
+				if (tmp == null)
+					tmp = coPhieu;
+				if (tmp.getBienDong() < coPhieu.getBienDong())
+					tmp = coPhieu;
+			}
+		return tmp;
+	}
+}
