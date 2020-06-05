d37ae603c01576bba30fc00241e9a6e633631319
long2906 <64420650+long2906@users.noreply.github.com>
2020-06-05 21:20:44 +0700

    Update FileTxt.java

--- FileTxt.java
+++ FileTxt.java
@@ -1,91 +1,91 @@
 package bai_tap_lon;
 
-import java.io.File;  
-import java.io.FileWriter;
-import java.io.IOException; 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
 
-public class FileTxt{
-	private String location = "D:\\HUST\\OOP\\Result.txt";
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JList;
+import javax.swing.JOptionPane;
 
-	private SanGiaoDich sgd = new SanGiaoDich();
-	private Tag tag;
-	
-	public FileTxt() {
-	}
+import java.awt.Font;
+import java.util.ArrayList;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JScrollPane;
 
-	public SanGiaoDich getSgd() {
-		return sgd;
-	}
+public class ChonTag extends JFrame {
 
-	public Tag getTag() {
-		return tag;
-	}
-	
-	public void setTag(Tag tag) {
-		this.tag = tag;
-	}
-	
-	//Má»Ÿ file
-	protected void openFile (String DuongDan) throws IOException {
-		FileReader fr = new FileReader(DuongDan);
-		String line = "";
 
-		try(BufferedReader br = new BufferedReader(fr);){
-		//GÃ¡n thuá»™c tÃ­nh
-			while ((line = br.readLine()) != null) {
-				String A[] = line.split(",");
-				sgd.getDSCP().add(new CoPhieu(A));
-			}
-			sgd.xuLiThongTin();
-		} catch(IOException e){
-		System.out.println("CÃ³ lá»—i");
-			fr.close();
-		}
-		sgd.addNhomCoPhieu(new NhomCoPhieu(sgd.getDSCP(),"NhÃ³m vá»‘n hÃ³a lá»›n","VHM","VIC","POW","GAS","VIC","VNM","SBT","HNG","GVR","BID","VRE","ITA","HAG","HPG","FLC"));
-		sgd.addNhomCoPhieu(new NhomCoPhieu(sgd.getDSCP(),"NhÃ³m dáº§u khÃ­","POW","GAS","PLX","PVD","PVT","PXS","PGD"));
-		sgd.addNhomCoPhieu(new NhomCoPhieu(sgd.getDSCP(),"NhÃ³m ngÃ¢n hÃ ng","BID","BVH","CTG","EIB","HDB","MBB","STB","TCB","TPB","VCB","VPB"));
-		sgd.addNhomCoPhieu(new NhomCoPhieu(sgd.getDSCP(),"NhÃ³m xÃ¢y dá»±ng vÃ  báº¥t Ä‘á»™ng sáº£n","VRE","ITA","VRE","VHM","VIC","REE","KDH","NVL","LGC","PDR","HPX"));
-		sgd.addNhomCoPhieu(new NhomCoPhieu(sgd.getDSCP(),"NhÃ³m khai khoÃ¡ng","PVD","NNC","KSB","DHA"));
-		sgd.addNhomCoPhieu(new NhomCoPhieu(sgd.getDSCP(),"NhÃ³m sáº£n xuáº¥t nÃ´ng nghiá»‡p","HNG","HAG","NSC","ASM","SSC"));
-		sgd.addNhomCoPhieu(new NhomCoPhieu(sgd.getDSCP(),"NhÃ³m sáº£n xuáº¥t vÃ  tiÃªu dÃ¹ng","BHN","DHG","GVR","HPG","MSN","SAB","VNM","KDC","PHR","GEX","PNJ"));
-		System.out.println("Truyá»�n dá»¯ liá»‡u thÃ nh cÃ´ng.");
-    }
-	
-	//Táº¡o file
-	protected void createNewFile() {
-		 File file = new File(location);
-			try {
-				if (file.createNewFile() == true) {
-					System.out.println("Ä�Ã£ táº¡o File!!!");
-					System.out.println("HÃ£y truy cáº­p: D:\\HUST\\OOP\\Result.txt ");
-				}else {
-					System.out.println("Táº¡o láº¡i thÃ nh cÃ´ng!!!");
+	private static final long serialVersionUID = 1L;
+	private JPanel ChonTag;
+	private ArrayList<String> tag = new ArrayList<String>();
+	private ArrayList <String> Arr;
+	//private JScrollPane scrollPane;
+
+	public void run() {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					ChonTag frame = new ChonTag();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
 				}
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
 			}
-	}
-	
-	protected String processFile() {
-		String s = "";
-		MauCau cau = new MauCau(sgd, tag);
-		s = cau.tongHopCau();
-		return s;
+		});
 	}
 
-	//Ghi vÃ o file
-	protected void writeToFile(String A) {
-		try {
-			FileWriter fileWriter = new FileWriter(location);
-			fileWriter.write(A);
-			fileWriter.close();
-		} catch (IOException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}	
-	}	
+	@SuppressWarnings("rawtypes")
+	public ChonTag() {
+		
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 350, 260);
+		ChonTag = new JPanel();
+		ChonTag.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(ChonTag);
+		setLocationRelativeTo(null);
+		
+		String[] T = {"Tăng nhiều nhất","Top 3 tăng","Giảm nhiều nhất","Top 3 giảm","Nước ngoài mua","Nước ngoài bán","Thông tin sàn giao dịch","Trạng thái sàn giao dịch","Xu hướng sàn giao dịch","Trạng thái nhóm dầu khí","Cổ phiếu nhóm dầu khí","Trạng thái nhóm ngân hàng","Cổ phiếu nhóm ngân hàng","So sánh giá với giá trần","Nhóm cổ phiếu tăng","Nhóm cổ phiếu tăng nhanh","Nhóm cổ phiếu giảm","Nhóm cổ phiếu giảm mạnh","Nhóm cổ phiếu ổn định","Nhóm cổ phiếu phân hóa","Đứng giá tham chiếu","Tự trụ tăng","Tăng hết biên độ","Diễn biến trái chiều","Đồng loạt mất điểm","Tuột dốc","Chìm trong sắc đỏ"};		
+		for (String index : T) {
+			tag.add(index);
+		}
+			
+		@SuppressWarnings("unchecked")
+		JList list = new JList(tag.toArray());
+		list.setSelectedIndex(1);
+		list.setFont(new Font("Arial", Font.PLAIN, 15));
+		list.setBounds(0, 0, 200, 376);		
+		ChonTag.add(list);
+		getContentPane().add(BorderLayout.CENTER, new JScrollPane(list));
+		
+		JButton chonButton = new JButton("Chọn");
+		chonButton.setFont(new Font("Tahoma", Font.BOLD, 15));
+		chonButton.setBounds(221, 36, 102, 41);
+		ChonTag.add(chonButton);
+		chonButton.addActionListener(new ActionListener() {
+			
+			@SuppressWarnings("unchecked")
+			public void actionPerformed(ActionEvent e) {
+				Arr = (ArrayList<String>) list.getSelectedValuesList();
+				System.out.println(Arr); //test
+				setVisible(false);
+				JOptionPane.showMessageDialog(null, "Chọn Tag thành công!!");
+			}
+		});
+		
+		JLabel lblNewLabel = new JLabel("Note:");
+		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 14));
+		lblNewLabel.setBounds(248, 138, 40, 17);
+		ChonTag.add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("Bạn có thể chọn nhiều Tag bằng cách giữ \"Ctrl+Chuột\"");
+		lblNewLabel_1.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		lblNewLabel_1.setBounds(235, 164, 79, 16);
+		ChonTag.add(lblNewLabel_1);
+		
+	}
 }
